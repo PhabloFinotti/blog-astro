c84ba4486e06d2e9a8355bbbd855a50e1806552a
Phablo Finotti <phablofinotti@gmail.com>
2024-05-18 12:13:58 -0300

    finished

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+// export async function GET(context) {
+//   return rss({
+//     title: 'Phablo Finotti | Blog',
+//     description: 'Minha jornada programando',
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//     customData: `<language>pt-br</language>`,
+//   });
+// }
